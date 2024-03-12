7aed90130c1be372a5bc139a6b5dc211926d5fe6
Iñigo Lopez de Heredia <inigo.lopezdeheredia@datadoghq.com>
2024-03-12 21:55:53 +0100

    fix yaml

--- scripts/build_content.py
+++ scripts/build_content.py
@@ -74,9 +74,13 @@ draft = false
 
 if __name__ == "__main__":
     sheet_id = os.getenv("SHEET_ID")
-    service_account_data = os.getenv("GSPREAD_SERVICE_ACCOUNT")
-    print("sheet_id: ", sheet_id)
+    service_account_data = os.getenv("GSPREAD_SERVICE_ACCOUNT", "")
+    print("sheet_id: ", sheet_id, "")
     csv_file = "spreadsheet.csv"
+
+    # Ensure service_account_data has outer quotes
+    if not service_account_data.startswith('"') or not service_account_data.endswith('"'):
+        service_account_data = f'"{service_account_data}"'
 
     # Open the public spreadsheet
     sheet = open_sheet(sheet_id, service_account_data)
